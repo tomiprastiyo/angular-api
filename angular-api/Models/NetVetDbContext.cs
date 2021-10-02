5977e49cce8ad40078a62cf6717e3e54994f3854
TomiPr <tomyprastiyo8@gmail.com>
2021-10-03 02:29:01 +0700

    [add] create NetVetDbContext and setting in startup

---
+++ angular-api/Models/NetVetDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using angular_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace angular_api.Models
+{
+    public class NetVetDbContext : DbContext
+    {
+        public NetVetDbContext(DbContextOptions<NetVetDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
--- angular-api/Startup.cs
+++ angular-api/Startup.cs
@@ -1,7 +1,9 @@
+using angular_api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,7 @@ namespace angular_api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "angular_api", Version = "v1" });
             });
+            services.AddDbContext<NetVetDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("NetVetDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
