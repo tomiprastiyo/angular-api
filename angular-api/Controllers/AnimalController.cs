618c0ca5dc663c59520e18b2a9040f7a236680ee
TomiPr <tomyprastiyo8@gmail.com>
2021-10-03 02:29:51 +0700

    [add] animal controller

---
+++ angular-api/Controllers/AnimalController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using angular_api.Models;
+
+namespace angular_api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Animal")]
+    public class AnimalController : Controller
+    {
+        private readonly NetVetDbContext _context;
+
+        public AnimalController(NetVetDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Animal
+        [HttpGet]
+        public IEnumerable<Animal> GetAnimal()
+        {
+            return _context.Animals;
+        }
+    }
+}
