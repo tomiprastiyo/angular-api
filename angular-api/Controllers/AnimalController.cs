6d2ced0ddef01f87e75c9de46a7b22d05ee63f5e
TomiPr <tomyprastiyo8@gmail.com>
2021-10-03 15:58:20 +0700

    [add] animal controller post put delete

--- angular-api/Controllers/AnimalController.cs
+++ angular-api/Controllers/AnimalController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using angular_api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace angular_api.Controllers
 {
@@ -23,6 +24,102 @@ namespace angular_api.Controllers
         public IEnumerable<Animal> GetAnimal()
         {
             return _context.Animals;
+        }
+
+        // GET: api/Animal/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAnimal([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var animal = await _context.Animals.SingleOrDefaultAsync(m => m.AnimalId == id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(animal);
+        }
+
+        // PUT: api/Animal/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAnimal([FromRoute] Guid AnimalId, [FromBody] Animal animal)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (AnimalId != animal.AnimalId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(AnimalId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Animal
+        [HttpPost]
+        public async Task<IActionResult> PostAnimal([FromBody] Animal animal)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            animal.AnimalId = Guid.NewGuid();
+
+            _context.Animals.Add(animal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAnimal", new { animalid = animal.AnimalId }, animal);
+        }
+
+        // DELETE: api/Animal/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal([FromRoute] Guid AnimalId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var animal = await _context.Animals.SingleOrDefaultAsync(m => m.AnimalId == AnimalId);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animals.Remove(animal);
+            await _context.SaveChangesAsync();
+
+            return Ok(animal);
+        }
+
+        private bool AnimalExists(Guid AnimalId)
+        {
+            return _context.Animals.Any(e => e.AnimalId == AnimalId);
         }
     }
 }
